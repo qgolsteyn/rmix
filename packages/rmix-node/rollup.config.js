e406812d107fd59f08265fe2ad295f1144686073
Quentin Golsteyn <qgolsteyn@gmail.com>
2021-03-19 14:20:23 -0700

    Update rollup configuration

--- packages/rmix-node/rollup.config.js
+++ packages/rmix-node/rollup.config.js
@@ -1,30 +1,31 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json";
 
 const input = "./src/index.ts";
 
-const plugins = [
-  typescript({
-    typescript: require("typescript"),
-  }),
-];
+const plugins = [typescript(), commonjs()];
 
 export default [
   {
+    external: ["lodash"],
     input,
     output: {
       file: pkg.module,
       format: "esm",
       sourcemap: true,
+      exports: "named",
     },
     plugins,
   },
   {
+    external: ["lodash"],
     input,
     output: {
       file: pkg.main,
       format: "cjs",
       sourcemap: true,
+      exports: "named",
     },
     plugins,
   },

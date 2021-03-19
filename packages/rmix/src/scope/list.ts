53303c703f3ce2f902a9340a1b7b8f448e43b963
Quentin Golsteyn <qgolsteyn@gmail.com>
2021-03-19 09:16:50 -0700

    Add filter and spread

--- packages/rmix/src/scope/list.ts
+++ packages/rmix/src/scope/list.ts
@@ -7,6 +7,13 @@ const list: Record<string, RmixDefinition> = {
       head: def.post((node) => ["_", node[0]]),
       tail: def.post((node) => ["_", ...node.slice(1)]),
       len: def.post((node) => ["_", node.length]),
+      spread: def.post((node) => {
+        if (Array.isArray(node[0])) {
+          return ["_", ...node[0]];
+        } else {
+          return ["_"];
+        }
+      }),
       get: def.post((node) => {
         if (typeof node[0] !== "number") {
           throw new Error("Invariant violation: element must be an integer");
@@ -17,6 +24,10 @@ const list: Record<string, RmixDefinition> = {
       map: def.post(([tag, ...list]) => [
         "_",
         ...list.map((item) => [tag, item]),
+      ]),
+      filter: def.post(([tag, ...list]) => [
+        "_",
+        ...list.map((item) => ["?", [tag, item], item]),
       ]),
       range: def.post(([end]) => {
         if (typeof end !== "number") {
